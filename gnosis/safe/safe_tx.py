4485195f48199326cdc2a226a1eb3205c66f00d3
BLAKPC\blak <zhiltsov.nick@gmail.com>
2022-12-14 09:48:16 +0300

    fix: harmony chainId=1 for EIP712

--- gnosis/safe/safe_tx.py
+++ gnosis/safe/safe_tx.py
@@ -90,7 +90,7 @@ class SafeTx:
         self.signatures = signatures or b""
         self._safe_nonce = safe_nonce and int(safe_nonce)
         self._safe_version = safe_version
-        self._chain_id = chain_id and int(chain_id)
+        self._chain_id = 1 #chain_id and int(chain_id)
 
         self.tx: Optional[TxParams] = None  # If executed, `tx` is set
         self.tx_hash: Optional[bytes] = None  # If executed, `tx_hash` is set
