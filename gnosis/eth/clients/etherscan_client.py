9535d0e4c3eee13f64037618412c59622cc2d0ac
msvstj <savastijonok@gmail.com>
2023-09-07 12:50:27 +0300

    feat: add linea etherscan client

--- gnosis/eth/clients/etherscan_client.py
+++ gnosis/eth/clients/etherscan_client.py
@@ -48,6 +48,8 @@ class EtherscanClient:
         EthereumNetwork.NEON_EVM_MAINNET: "https://neonscan.org",
         EthereumNetwork.SEPOLIA: "https://sepolia.etherscan.io",
         EthereumNetwork.ZKSYNC_V2: "https://explorer.zksync.io/",
+        EthereumNetwork.LINEA: "https://www.lineascan.build",
+        EthereumNetwork.LINEA_TESTNET: "https://goerli.lineascan.build",
     }
 
     NETWORK_WITH_API_URL = {
@@ -76,6 +78,8 @@ class EtherscanClient:
         EthereumNetwork.NEON_EVM_MAINNET: "https://api.neonscan.org",
         EthereumNetwork.SEPOLIA: "https://api-sepolia.etherscan.io",
         EthereumNetwork.ZKSYNC_V2: "https://block-explorer-api.mainnet.zksync.io/",
+        EthereumNetwork.LINEA: "https://api.lineascan.build",
+        EthereumNetwork.LINEA_TESTNET: "https://api-testnet.lineascan.build",
     }
     HTTP_HEADERS = {
         "User-Agent": "curl/7.77.0",
