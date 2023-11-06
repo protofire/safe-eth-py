1c61b36bdbc50c90a114e6540ae14b80f2228d0f
nick8319 <zhiltsov.nick@gmail.com>
2023-11-06 12:35:14 +0100

    feat: add scroll trx service urls

--- gnosis/safe/api/transaction_service_api.py
+++ gnosis/safe/api/transaction_service_api.py
@@ -31,6 +31,8 @@ class TransactionServiceApi(SafeBaseAPI):
         EthereumNetwork.POLYGON: "https://safe-transaction-polygon.safe.global",
         EthereumNetwork.ZKSYNC_V2: "https://safe-transaction-zksync.safe.global",
         EthereumNetwork.POLYGON_ZKEVM: "https://safe-transaction-zkevm.safe.global",
+        EthereumNetwork.SCROLL: "https://transaction.staging.safe.scroll.xyz",
+        EthereumNetwork.SCROLL_SEPOLIA_TESTNET: "https://transaction-sepolia.staging.safe.scroll.xyz",
     }
 
     @classmethod
