dac46d797b1d1c9bc3e499590f34810e2dc1e0a8
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-01-31 13:09:25 +0700

    feat: add Zora Mainnet 1.3.0 safe contracts deployed via safe singleton factory

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -527,8 +527,26 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         )
     ],
     EthereumNetwork.ZORA_NETWORK: [
-        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 11932, "1.3.0+L2"),
-        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 11934, "1.3.0"),
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            9677661,
+            "1.3.0+L2",
+        ),  # safe singleton address
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            11932,
+            "1.3.0+L2",
+        ),  # default singleton address
+        (
+            "0x69f4D1788e39c87893C980c06EdF4b7f686e2938",
+            9677757,
+            "1.3.0",
+        ),  # safe singleton address
+        (
+            "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+            11934,
+            "1.3.0",
+        ),  # default singleton address
     ],
     EthereumNetwork.ZKSYNC_ALPHA_TESTNET: [
         ("0x1727c2c531cf966f902E5927b98490fDFb3b2b70", 8619879, "1.3.0+L2"),
@@ -1067,7 +1085,14 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         )
     ],
     EthereumNetwork.ZORA_NETWORK: [
-        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 11914),  # v1.3.0
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            9677950,
+        ),  # v1.3.0 safe singleton address
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            11914,
+        ),  # v1.3.0 default singleton address
     ],
     EthereumNetwork.ZKSYNC_ALPHA_TESTNET: [
         ("0xDAec33641865E4651fB43181C6DB6f7232Ee91c2", 8619849),  # v1.3.0
