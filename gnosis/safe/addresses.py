de1714fc962fb283478e388ce6a8f1c952440ba6
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-04-17 20:56:00 +0800

    feat: add 1.3.0 safe contracts for Arbitrum Blueberry

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -1063,6 +1063,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1028176, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 1028180, "1.3.0"),
     ],
+    EthereumNetwork.ARBITRUM_BLUEBERRY_TESTNET: [
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 19372, "1.3.0+L2"),
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 0, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -1710,5 +1714,8 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],  # BOB Testnet
     EthereumNetwork.OP_CELESTIA_RASPBERRY_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1028156),  # v1.3.0
+    ],
+    EthereumNetwork.ARBITRUM_BLUEBERRY_TESTNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 19364),  # v1.3.0
     ],
 }
