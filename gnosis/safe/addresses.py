0d987e750f139f7cda466dca2a240187df61ae60
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-05-14 20:35:43 +0300

    feat: add Cyber Mainnet

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -1127,6 +1127,28 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0",
         ),  # safe singleton address
     ],
+    EthereumNetwork.CYBER_MAINNET: [
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            1098954,
+            "1.3.0+L2",
+        ),  # default singleton address
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            0,
+            "1.3.0+L2",
+        ),  # safe singleton address
+        (
+            "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+            1098959,
+            "1.3.0",
+        ),  # default singleton address
+        (
+            "0x69f4D1788e39c87893C980c06EdF4b7f686e2938",
+            0,
+            "1.3.0",
+        ),  # safe singleton address
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -1806,5 +1828,15 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
             260112,
         ),  # v1.3.0 safe singleton address
+    ],
+    EthereumNetwork.CYBER_MAINNET: [
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            1098959,
+        ),  # v1.3.0 safe singleton address
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            1098904,
+        ),  # v1.3.0 default singleton address
     ],
 }
