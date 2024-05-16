31759f0ad6cfba54fe4ebfadc30760300c7e2cf6
Den Smalonski <dzianis.smalonski@protofire.io>
2024-05-16 16:48:20 +0200

    feat: Add TAIKO_HEKLA_L2 network addresses

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -1193,6 +1193,18 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0",
         ),  # safe singleton address
     ],
+    EthereumNetwork.LISK: [
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            158815,
+            "1.3.0+L2",
+        ),  # default singleton address
+        (
+            "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+            158860,
+            "1.3.0",
+        ),  # default singleton address
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -1902,5 +1914,11 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
             139932,
         ),  # v1.3.0 default singleton address
+    ],
+    EthereumNetwork.TAIKO_HEKLA_L2: [
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            158794,
+        ),  # v1.3.0 safe singleton address
     ],
 }
