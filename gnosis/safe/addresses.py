b52cedecf8fd01fb0c7c9242c7bf76cc06bbfcaa
Den Smalonski <dzianis.smalonski@protofire.io>
2024-05-29 16:44:34 +0200

    add taiko mainnet

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -1353,6 +1353,18 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0",
         ),  # default singleton address
     ],
+    EthereumNetwork.TAIKO_L2: [
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            7376,
+            "1.3.0+L2",
+        ),  # default singleton address
+        (
+            "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+            7377,
+            "1.3.0",
+        ),  # default singleton address
+    ],
     EthereumNetwork.SEI_NETWORK: [
         (
             "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
@@ -2148,6 +2160,12 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         (
             "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
             158794,
+        ),  # v1.3.0 default singleton address
+    ],
+    EthereumNetwork.TAIKO_L2: [
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            7366,
         ),  # v1.3.0 default singleton address
     ],
     EthereumNetwork.SEI_NETWORK: [
