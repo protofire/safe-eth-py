d5e37dc65dffa6aaaf0751e854706b262a0f3701
Denis Smolonski <denissmolonski@gmail.com>
2024-07-18 10:50:30 +0100

    feat: add autonomous testnet

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -1571,6 +1571,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 1649, "1.3.0"),  # v1.3.0
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1648, "1.3.0+L2"),  # v1.3.0+L2
     ],
+    EthereumNetwork.AUTONOMOUS_TESTNET: [
+      ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 1196806, "1.3.0"),  # v1.3.0
+      ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 1196805, "1.3.0+L2"),  # v1.3.0+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -2360,4 +2364,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.DODOCHAIN_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1641),  # v1.3.0
     ],
+    EthereumNetwork.AUTONOMOUS_TESTNET: [
+      ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 1196789),  # v1.3.0
+    ]
 }
