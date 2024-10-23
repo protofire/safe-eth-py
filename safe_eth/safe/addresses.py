90a2e7b8e0e8fe07706e06a535a4633805ee9368
msvstj <savastijonok@gmail.com>
2024-10-23 11:04:47 +0300

    feat: support mode safe singleton deployed addresses

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1013,14 +1013,28 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 1514966, "1.3.0"),
     ],
     EthereumNetwork.MODE: [
-        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 2610515, "1.3.0+L2"),
-        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 2610520, "1.3.0"),
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 6524841, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 6524836, "1.4.1+L2"),
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 6524841, "1.4.1"),
         (
-            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
-            6524836,
-            "1.4.1+L2",
-        ),  # v1.4.1+L2
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            2610515,
+            "1.3.0+L2",
+        ),  # default singleton address
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            7457753,
+            "1.3.0+L2",
+        ),  # safe singleton address
+        (
+            "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+            2610520,
+            "1.3.0",
+        ),  # default singleton address
+        (
+            "0x69f4D1788e39c87893C980c06EdF4b7f686e2938",
+            7457757,
+            "1.3.0",
+        ),  # safe singleton address
     ],
     EthereumNetwork.MODE_TESTNET: [
         (
@@ -2305,8 +2319,15 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 1514954),  # v1.3.0
     ],
     EthereumNetwork.MODE: [
-        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 2610484),  # v1.3.0
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 6524805),  # v1.4.1
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            7457728,
+        ),  # v1.3.0 safe singleton address
+        (
+            "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
+            2610484,
+        ),  # v1.3.0 default singleton address
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 2610484),  # v1.4.1
     ],
     EthereumNetwork.MODE_TESTNET: [
         (
