aae71923b39e4cd7f912bdad04eb13151e0c2440
msvstj <savastijonok@gmail.com>
2024-10-23 11:16:26 +0300

    fix: 1.4.1 version block number for mode

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2327,7 +2327,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
             2610484,
         ),  # v1.3.0 default singleton address
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 2610484),  # v1.4.1
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 6524805),  # v1.4.1
     ],
     EthereumNetwork.MODE_TESTNET: [
         (
