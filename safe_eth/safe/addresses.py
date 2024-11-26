f31460e3d8f3502b72f658720faf200d6869cbd1
msvstj <savastijonok@gmail.com>
2024-11-26 15:42:27 +0200

    feat: story odyssey addresses

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1909,6 +1909,24 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 836417, "1.4.1"),  # v1.4.1
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 836412, "1.4.1+L2"),  # v1.4.1+L2
     ],
+    EthereumNetwork.STORY_ODYSSEY: [
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            340800,
+            "1.3.0+L2",
+        ),  # safe singleton address
+        (
+            "0x69f4D1788e39c87893C980c06EdF4b7f686e2938",
+            340803,
+            "1.3.0",
+        ), 
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 340907, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            340904,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -2865,5 +2883,12 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 836921),  # v1.3.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 836863),  # v1.3.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 836381),  # v1.4.1
+    ],
+    EthereumNetwork.STORY_ODYSSEY: [
+        (
+            "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
+            340781,
+        ),  # v1.3.0 safe singleton address
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 340886),  # v1.4.1
     ],
 }
