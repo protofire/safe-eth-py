b1209cb1809199373f8a7efc26e9793ca82b9f55
msvstj <savastijonok@gmail.com>
2024-12-18 17:42:36 +0200

    feat: support filecoin 1.3.0 safe-ver contracts

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1870,6 +1870,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ),  # v1.3.0+L2
     ],
     EthereumNetwork.FILECOIN_MAINNET: [
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 4541183, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 4541180, "1.3.0+L2"),  # v1.3.0+L2
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 4155380, "1.4.1"),  # v1.4.1
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 4155378, "1.4.1+L2"),  # v1.4.1+L2
     ],
@@ -2824,6 +2826,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 3007535),  # v1.3.0
     ],
     EthereumNetwork.FILECOIN_MAINNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 4541142),  # v1.3.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 4155364),  # v1.4.1
     ],
     EthereumNetwork.FILECOIN_CALIBRATION_TESTNET: [
