d546c2a213d9f7488d9b4b0cc1e0acd2e1236b20
ajimeno04 <jimenobeltran11@gmail.com>
2024-12-23 10:51:02 +0100

    feat: add abstract networks

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -789,6 +789,26 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0+L2",
         )
     ],
+        EthereumNetwork.ABSTRACT: [
+        ("0x1727c2c531cf966f902E5927b98490fDFb3b2b70", 13508, "1.3.0+L2"),
+        ("0xB00ce5CCcdEf57e539ddcEd01DF43a13855d9910", 13509, "1.3.0"),
+        ("0xC35F063962328aC65cED5D4c3fC5dEf8dec68dFa", 54402, "1.4.1"),  # v1.4.1
+        (
+            "0x610fcA2e0279Fa1F8C00c8c2F71dF522AD469380",
+            54403,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
+    ],
+    EthereumNetwork.ABSTRACT_TESTNET: [
+        ("0x1727c2c531cf966f902E5927b98490fDFb3b2b70", 2207769, "1.3.0+L2"),
+        ("0xB00ce5CCcdEf57e539ddcEd01DF43a13855d9910", 2207773, "1.3.0"),
+        ("0xC35F063962328aC65cED5D4c3fC5dEf8dec68dFa", 2209140, "1.4.1"),  # v1.4.1
+        (
+            "0x610fcA2e0279Fa1F8C00c8c2F71dF522AD469380",
+            2209146,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
+    ],
     EthereumNetwork.ZORA: [
         (
             "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
@@ -2352,6 +2372,14 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.ETHEREUM_CLASSIC: [
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 15904946),  # v1.3.0
+    ],
+    EthereumNetwork.ABSTRACT: [
+        ("0xDAec33641865E4651fB43181C6DB6f7232Ee91c2", 2207746),  # v1.3.0
+        ("0xc329D02fd8CB2fc13aa919005aF46320794a8629", 54392),  # v1.4.1
+    ],
+    EthereumNetwork.ABSTRACT_TESTNET: [
+        ("0xDAec33641865E4651fB43181C6DB6f7232Ee91c2", 2207746),  # v1.3.0
+        ("0xc329D02fd8CB2fc13aa919005aF46320794a8629", 2206856),  # v1.4.1
     ],
     EthereumNetwork.MORDOR_TESTNET: [
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 6333172),  # v1.3.0
