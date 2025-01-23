7b524b70c149d4399f873ad921cc9e717ac9ac99
ajimeno04 <jimenobeltran11@gmail.com>
2025-01-23 19:02:47 +0100

    feat: update blocknumber for berachain

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -79,10 +79,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.BERACHAIN: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 0, "1.3.0+L2"), # default singleton address
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 0, "1.3.0"), # default singleton address
-        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 0, "1.3.0+L2"),
-        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 0, "1.3.0"),
-        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 0, "1.4.1+L2"),
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 0, "1.4.1"),
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 45838, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 45842, "1.3.0"),
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 40887, "1.4.1+L2"),
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 40883, "1.4.1"),
     ],
     EthereumNetwork.GOERLI: [
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 9134479, "1.4.1+L2"),
@@ -2080,8 +2080,8 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.BERACHAIN: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 0),  # v1.3.0 default singleton address
-        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 0),  # v1.3.0
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 0),  # v1.4.1
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 45810),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 40852),  # v1.4.1
     ],
     EthereumNetwork.GOERLI: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 8681525),  # v1.4.1
