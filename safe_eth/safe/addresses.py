befc80b459c9a05d6fd604e2d77292501dbfd2c5
goxibyte <denissmolonski@gmail.com>
2025-01-24 18:08:55 +0100

    feat (addresses): add boba sepolia

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -44,6 +44,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x8942595A2dC5181Df0465AF0D7be08c8f23C93af", 6766257, "0.1.0"),
         ("0xAC6072986E985aaBE7804695EC2d8970Cf7541A2", 6569433, "0.0.2"),
     ],
+    EthereumNetwork.BOBA_SEPOLIA: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 8926694, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 8927245, "1.3.0"),
+    ],
     EthereumNetwork.IMMUTABLE_ZKEVM_DEVNET: [
         ("0xE418eBDFBa5bf9d512fdF0dd19F700de60a49959", 1528994, "1.3.0+L2"),  # temporary-deployment
         ("0x0e560eDaa22849d78B6aa7d60CBEFC7Dcf540818", 1529000, "1.3.0"),  # temporary-deployment
@@ -2499,6 +2503,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.EDGEWARE_EDGEEVM_MAINNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 18176812),  # v1.3.0
+    ],
+    EthereumNetwork.BOBA_SEPOLIA: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 8926647),  # v1.3.0
     ],
     EthereumNetwork.LINEA: [
         (
