8c134a3b7120aa9ca9f56db7870cb519eb9f04e4
ajimeno04 <jimenobeltran11@gmail.com>
2025-02-27 13:43:08 +0100

    Deletion: INKCHAIN_TESTNET and KAKAROT_STARKNET_SEPOLIA network entries duplicated

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -78,12 +78,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x8942595A2dC5181Df0465AF0D7be08c8f23C93af", 3392692, "0.1.0"),
         ("0x2727D69C0BD14B1dDd28371B8D97e808aDc1C2f7", 3055781, "0.0.2"),
     ],
-    EthereumNetwork.INKCHAIN_TESTNET: [
-        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 0, "1.3.0+L2"),
-        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 0, "1.3.0"),
-        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 0, "1.4.1+L2"),
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 0, "1.4.1"),
-    ],
+   
     EthereumNetwork.BERACHAIN: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 0, "1.3.0+L2"), # default singleton address
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 0, "1.3.0"), # default singleton address
@@ -2443,10 +2438,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x50e55Af101C777bA7A1d560a774A82eF002ced9F", 5423494),
         ("0x12302fE9c02ff50939BaAaaf415fc226C078613C", 4110083),
     ],
-    EthereumNetwork.INKCHAIN_TESTNET: [
-        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 0),  # v1.3.0
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 0),  # v1.4.1
-    ],
+
     EthereumNetwork.BERACHAIN: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 0),  # v1.3.0 default singleton address
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 45810),  # v1.3.0
