0542fe3621c1bd4a27a33a24eb606ecf92f85ba0
msvstj <savastijonok@gmail.com>
2025-03-25 11:33:00 +0200

    feat: expchain testnet data

--- safe_eth/eth/clients/blockscout_client.py
+++ safe_eth/eth/clients/blockscout_client.py
@@ -222,6 +222,7 @@ class BlockscoutClient:
         EthereumNetwork.EVM_ON_FLOW: "https://evm.flowscan.io/api/v2",
         EthereumNetwork.EVM_ON_FLOW_TESTNET: "https://evm-testnet.flowscan.io/api/v2",
         EthereumNetwork.BIRDLAYER: "https://scan.birdlayer.xyz/api/v2",
+        EthereumNetwork.EXPCHAIN_TESTNET: "https://blockscout-testnet.expchain.ai/api/v2",
     }
 
     def __init__(
