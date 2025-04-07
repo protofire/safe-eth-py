e88c48d9570e6b1ad76a3ed748d8a264e359c4d4
goxibyte <denissmolonski@gmail.com>
2025-04-07 15:32:34 +0200

    feat: add berachain bepolia addresses

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2499,6 +2499,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 0, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 0, "1.3.0"),
     ],
+    EthereumNetwork.BERACHAIN_BEPOLIA: [
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 2068001, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 2068009, "1.4.1+L2"),  # v1.4.1+L2
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 2070069, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 2070087, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -3716,6 +3722,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.PYROPE_TESTNET: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 382695),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 629498),  # v1.3.0 eip155
+    ],
+    EthereumNetwork.BERACHAIN_BEPOLIA: [
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 2067958),  # v1.4.1
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 2069978),  # v1.3.0 eip155
     ],
 }
 
