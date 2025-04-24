f85a377ca1cc581436a7b0648d593b24d8892c23
goxibyte <denissmolonski@gmail.com>
2025-04-24 16:49:20 +0200

    feat: add Hyper EVM

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -3773,7 +3773,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 2067958),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 2069978),  # v1.3.0 eip155
     ],
-    EthereumNetwork.UNICHAIN: [
+    EthereumNetwork.HYPEREVM: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 60597),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 80104),  # v1.3.0 eip155
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 19591),  # v1.3.0 canonical
