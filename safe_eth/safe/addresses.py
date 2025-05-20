5b21d8cd8afa3b883b231d5f8ec77ec62432ef97
goxibyte <denissmolonski@gmail.com>
2025-05-20 11:24:43 +0200

    fix(addresses): Story Aeneid

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2388,16 +2388,16 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 75, "1.4.1+L2"),  # v1.4.1+L2
     ],
     EthereumNetwork.STORY_AENEID: [
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 2462519, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 2462515, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 973663, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 974369, "1.4.1+L2"),  # v1.4.1+L2
+    ],
+    EthereumNetwork.WATERFALL_9_TEST_NETWORK: [
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 973663, "1.4.1"),  # v1.4.1
         (
             "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
-            974369,),
-    ],
-    EthereumNetwork.WATERFALL_9_TEST_NETWORK: [
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 7329851, "1.4.1"),  # v1.4.1
-        (
-            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
-            7329879,
+            974369,
             "1.4.1+L2",
         ),  # v1.4.1+L2
     ],
@@ -3761,6 +3761,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 68),  # v1.4.1
     ],
     EthereumNetwork.STORY_AENEID: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 2462491),  # v1.3.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 973637),  # v1.4.1
     ],
     EthereumNetwork.UNICHAIN: [
