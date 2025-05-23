9fc8e87d37c1adabe48825e4648312bd0e14e661
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-05-23 22:53:28 +0300

    refactor: remove duplicated Filecoin addresses

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2079,14 +2079,6 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 48, "1.4.1"),  # v1.4.1
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 47, "1.4.1+L2"),  # v1.4.1+L2
     ],
-    EthereumNetwork.FILECOIN_MAINNET: [
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 4155380, "1.4.1"),  # v1.4.1
-        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 4155378, "1.4.1+L2"),  # v1.4.1+L2
-    ],
-    EthereumNetwork.FILECOIN_CALIBRATION_TESTNET: [
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 1854685, "1.4.1"),  # v1.4.1
-        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 1839902, "1.4.1+L2"),  # v1.4.1+L2
-    ],
     EthereumNetwork.BERACHAIN_CARTIO: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 100490, "1.3.0"),  # v1.3.0
         (
