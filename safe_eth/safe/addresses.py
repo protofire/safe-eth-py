8536bc260511c12540a92612e6640412dea8c215
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-05-23 22:48:44 +0300

    refactor: remove duplicated Berachain addresses

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -80,15 +80,6 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x8942595A2dC5181Df0465AF0D7be08c8f23C93af", 3392692, "0.1.0"),
         ("0x2727D69C0BD14B1dDd28371B8D97e808aDc1C2f7", 3055781, "0.0.2"),
     ],
-   
-    EthereumNetwork.BERACHAIN: [
-        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 0, "1.3.0+L2"), # default singleton address
-        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 0, "1.3.0"), # default singleton address
-        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 45838, "1.3.0+L2"),
-        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 45842, "1.3.0"),
-        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 40887, "1.4.1+L2"),
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 40883, "1.4.1"),
-    ],
     EthereumNetwork.GOERLI: [
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 9134479, "1.4.1+L2"),
         ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 9134480, "1.4.1"),
@@ -2547,12 +2538,6 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B", 5590757),
         ("0x50e55Af101C777bA7A1d560a774A82eF002ced9F", 5423494),
         ("0x12302fE9c02ff50939BaAaaf415fc226C078613C", 4110083),
-    ],
-
-    EthereumNetwork.BERACHAIN: [
-        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 0),  # v1.3.0 default singleton address
-        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 45810),  # v1.3.0
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 40852),  # v1.4.1
     ],
     EthereumNetwork.GOERLI: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 8681525),  # v1.4.1
