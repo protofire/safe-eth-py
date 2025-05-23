0dc8f885875e6b9976f928e38243c91af7f24475
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-05-24 00:06:21 +0300

    refactor: rename KAKAROT with KAKAROT_STARKNET_SEPOLIA

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1050,7 +1050,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 85332, "1.3.0"),
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 85331, "1.3.0+L2"),
     ],
-    EthereumNetwork.KAKAROT: [
+    EthereumNetwork.KAKAROT_STARKNET_SEPOLIA: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 239183, "1.3.0"),
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 239180, "1.3.0+L2"),
     ],
@@ -2723,7 +2723,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.KAIA_MAINNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 93506870),  # v1.3.0
     ],
-    EthereumNetwork.KAKAROT: [
+    EthereumNetwork.KAKAROT_STARKNET_SEPOLIA: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",  239112),  # v1.3.0
     ],
     EthereumNetwork.MILKOMEDA_A1_TESTNET: [
