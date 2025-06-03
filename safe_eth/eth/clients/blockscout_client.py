5835cf959e42765da793dcfa73013a665052a594
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-06-03 18:31:05 +0300

    feat: update SONEIUM_TESTNET_MINATO name and add blockscout client

--- safe_eth/eth/clients/blockscout_client.py
+++ safe_eth/eth/clients/blockscout_client.py
@@ -117,7 +117,8 @@ class BlockscoutClient:
         EthereumNetwork.BIRDLAYER: "https://scan.birdlayer.xyz/api/v2",
         EthereumNetwork.EXPCHAIN_TESTNET: "https://blockscout-testnet.expchain.ai/api/v2",
         EthereumNetwork.ETHEREUM_HOODI: "https://eth-hoodi.blockscout.com/api/v2/",
-        EthereumNetwork.STORY: "https://storyscan.io/api/v2"
+        EthereumNetwork.STORY: "https://storyscan.io/api/v2",
+        EthereumNetwork.SONEIUM_TESTNET_MINATO: "https://soneium-minato.blockscout.com/api/v2/",
     }
 
     def __init__(
--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -411,7 +411,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.4.1+L2",
         ),  # v1.4.1+L2
     ],
-    EthereumNetwork.MINATO: [
+    EthereumNetwork.SONEIUM_TESTNET_MINATO: [
         (
             "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
             0,
@@ -432,10 +432,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             1835127,
             "1.3.0",
         ),  # default singleton address
-        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 0, "1.4.1"),  # v1.4.1
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 3900870, "1.4.1"),  # v1.4.1
         (
             "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
-            0,
+            3900875,
             "1.4.1+L2",
         ),  # v1.4.1+L2
     ],
@@ -3165,7 +3165,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
       ), 
       ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 7688600),  # v1.4.1
     ],
-    EthereumNetwork.MINATO: [
+    EthereumNetwork.SONEIUM_TESTNET_MINATO: [
         (
             "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
             2337245,
@@ -3174,7 +3174,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
             "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",
             1835098,
         ),  # v1.3.0 default singleton address
-        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 0),  # v1.4.1
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 3900839),  # v1.4.1
     ],
     EthereumNetwork.MODE_TESTNET: [
         (
