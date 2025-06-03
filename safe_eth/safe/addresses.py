cecd9cafdd9b7841e32eb061796cb203bfbdb3dd
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-06-03 17:32:56 +0300

    feat: add contracts for NIBIRU networks

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2534,6 +2534,38 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0+L2",
         ),  # v1.3.0+L2
     ],
+    EthereumNetwork.NIBIRU_CATACLYSM_1: [
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 21722399, "1.3.0"),  # v1.3.0
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            21722389,
+            "1.3.0+L2",
+        ),  # v1.3.0+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 19868217, "1.3.0"),  # v1.3.0
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            19868594,
+            "1.3.0+L2",
+        ),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 19869113, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            19869158,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
+    ],
+    EthereumNetwork.NIBIRU_TESTNET_2: [
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 1126361, "1.3.0"),  # v1.3.0
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            1126358,
+            "1.3.0+L2",
+        ),  # v1.3.0+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 675154, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 675137, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 674987, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 674995, "1.4.1+L2"),  # v1.4.1+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -3645,7 +3677,6 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ),  # v1.3.0 safe singleton address
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 621073),  # v1.4.1
     ],
-
     EthereumNetwork.SONEIUM: [
         (
             "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",
@@ -3746,6 +3777,16 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 60597),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 80104),  # v1.3.0 eip155
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 19591),  # v1.3.0 canonical
+    ],
+    EthereumNetwork.NIBIRU_CATACLYSM_1: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 21722329),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 19868328),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 19858911),  # v1.4.1
+    ],
+    EthereumNetwork.NIBIRU_TESTNET_2: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1126337),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 675078),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 674772),  # v1.4.1
     ],
 }
 
