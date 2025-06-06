1084869285ae16f784aa87399b11b44b204bd107
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-06-06 18:48:00 +0300

    feat: add contracts for BOTANIX_MAINNET network

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1426,6 +1426,20 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0+L2",
         ),  # v1.3.0+L2
     ],
+    EthereumNetwork.BOTANIX_MAINNET: [
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 235980, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            235983,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 235718, "1.3.0"),  # v1.3.0
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            235717,
+            "1.3.0+L2",
+        ),  # v1.3.0+L2
+    ],
     EthereumNetwork.BLAST_SEPOLIA_TESTNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1087958, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 1087964, "1.3.0"),
@@ -3291,6 +3305,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 165422),  # v1.4.1
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 1368795),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 1368703),  # v1.3.0
+    ],
+    EthereumNetwork.BOTANIX_MAINNET: [
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 235940),  # v1.4.1
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 235706),  # v1.3.0
     ],
     EthereumNetwork.BLAST_SEPOLIA_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1087898),  # v1.3.0
