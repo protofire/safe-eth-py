20f5ce8c497753ff850b76841b6d6ebe40783f1a
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-06-12 11:58:37 +0300

    feat: add TAC_MAINNET chain with addresses

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2644,6 +2644,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 2070087, "1.3.0"),  # v1.3.0
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 2070069, "1.3.0+L2"),  # v1.3.0+L2
     ],
+    EthereumNetwork.TAC_MAINNET: [
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 18001, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 18003, "1.4.1+L2"),  # v1.4.1+L2
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 18477, "1.3.0"),  # v1.3.0 canonical
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 18474, "1.3.0+L2"),  # v1.3.0+L2 canonical
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 18286, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 18283, "1.3.0+L2"),  # v1.3.0+L2
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -3873,6 +3881,11 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 2067958),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 2069978),  # v1.3.0 eip155
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1440031),  # v1.3.0 canonical
+    ],
+    EthereumNetwork.TAC_MAINNET: [
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 17977),  # v1.4.1
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 18262),  # v1.3.0 eip155
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 18453),  # v1.3.0 canonical
     ],
 }
 
