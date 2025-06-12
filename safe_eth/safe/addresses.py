daa2923b6c100a1e2a453f4d008da6ec9405af99
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-06-12 19:26:59 +0300

    feat: add TAC_TURIN

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2652,6 +2652,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 18286, "1.3.0"),  # v1.3.0
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 18283, "1.3.0+L2"),  # v1.3.0+L2
     ],
+    EthereumNetwork.TAC_TURIN: [
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 2231707, "1.4.1+L2"),
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 2231705, "1.4.1"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -3886,6 +3890,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 17977),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 18262),  # v1.3.0 eip155
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 18453),  # v1.3.0 canonical
+    ],
+    EthereumNetwork.TAC_TURIN: [
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 2231663),  # v1.4.1
     ],
 }
 
