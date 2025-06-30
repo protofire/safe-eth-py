3deaba05e09ee5c27fcf47c249a677feea7928f8
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2025-06-30 19:48:11 +0300

    Add Arachnid addresses for version 1.3.0 and 1.3.0+L2 in addresses.py for BOTANIX_MAINNET

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -1439,6 +1439,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             235717,
             "1.3.0+L2",
         ),  # v1.3.0+L2
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 529882, "1.3.0"),  # v1.3.0
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 529880, "1.3.0+L2"),  # v1.3.0+L2
     ],
     EthereumNetwork.BLAST_SEPOLIA_TESTNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1087958, "1.3.0+L2"),
@@ -3333,6 +3335,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.BOTANIX_MAINNET: [
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 235940),  # v1.4.1
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 235706),  # v1.3.0
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 529867),  # v1.3.0
     ],
     EthereumNetwork.BLAST_SEPOLIA_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1087898),  # v1.3.0
