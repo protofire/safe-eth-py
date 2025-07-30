075bb567b2ecac08d61ee07afe5a7b843f069f3f
JohnPochta <johnpochta1@gmail.com>
2025-07-30 14:37:11 +0100

    feat(add-sei-testnet): sei testnet added

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -209,6 +209,30 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0",
         ),  # default singleton address
     ],
+    EthereumNetwork.SEI_TESTNET: [
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 188519568, "1.4.1+L2"),
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 188512419, "1.4.1"),
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            188520010,
+            "1.3.0+L2",
+        ),  # safe singleton address
+        (
+            "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
+            188520300,
+            "1.3.0+L2",
+        ),  # default singleton address
+        (
+            "0x69f4D1788e39c87893C980c06EdF4b7f686e2938",
+            188520512,
+            "1.3.0",
+        ),  # safe singleton address
+        (
+            "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552",
+            154674365,
+            "1.3.0",
+        ),  # default singleton address
+    ],
     EthereumNetwork.MUMBAI: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 13736914, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 13736914, "1.3.0"),
@@ -2791,6 +2815,11 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 517662),  # v1.3.0 default singleton address
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 519172),  # v1.3.0 safe singleton address
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 521732),  # v1.4.1
+    ],
+    EthereumNetwork.SEI_TESTNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 188518516),  # v1.3.0 default singleton address
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 188519031),  # v1.3.0 safe singleton address
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 188512419),  # v1.4.1
     ],
     EthereumNetwork.ARBITRUM_ONE: [
         (
