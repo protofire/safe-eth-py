94d04dc9b7d32fdc9c31262c720019765566b598
Den <dzianis.smalonski@protofire.io>
2025-08-08 13:56:15 +0200

    Add autonomus mainnet

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2071,6 +2071,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 416924, "1.4.1+L2"),  # v1.4.1+L2
     
     ],
+    EthereumNetwork.AUTONOMYS_MAINNET: [
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 42000, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 42000, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 42000, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 42000, "1.4.1+L2"),  # v1.4.1+L2
+    ],
     EthereumNetwork.GNOSIS_CHIADO_TESTNET: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 117845, "1.3.0"),  # v1.3.0
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 117838, "1.3.0+L2"),  # v1.3.0+L2
@@ -3681,6 +3687,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1676306),  # v1.3.0
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 418267),  # v1.3.0
         ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 416910),  # v1.4.1
+    ],
+    EthereumNetwork.AUTONOMYS_MAINNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 42000),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 42000),  # v1.4.1
     ],
     EthereumNetwork.GNOSIS_CHIADO_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 117835),  # v1.3.0
