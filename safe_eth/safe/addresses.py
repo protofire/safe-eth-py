aac4f2c85cf4f1052d9289c63a40c84bfa3dadae
Emosaddd <pl.maksim.krukovich@gmail.com>
2025-12-17 14:32:47 +0100

    feat: add Sophon OS Testnet

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2952,6 +2952,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xB00ce5CCcdEf57e539ddcEd01DF43a13855d9910", 13509, "1.3.0"),  # v1.3.0
         ("0x1727c2c531cf966f902E5927b98490fDFb3b2b70", 13508, "1.3.0+L2"),  # v1.3.0+L2
     ],
+    EthereumNetwork.SOPHON_ZKSYNC_OS_TESTNET: [
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 83249, "1.3.0"),  # v1.3.0
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 83248, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 20060, "1.3.0"),  # v1.3.0
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 20059, "1.3.0+L2"),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 20705, "1.4.1"),  # v1.4.1
+        ("0x29fcB43b46531BcA003ddC8FCB67FFE91900C762", 20706, "1.4.1+L2"),  # v1.4.1+L2
+    ],
     EthereumNetwork.PLASMA_TESTNET: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 517714, "1.3.0"),  # v1.3.0
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 517707, "1.3.0+L2"),  # v1.3.0+L2
@@ -4535,6 +4543,11 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.SOPHON_TESTNET: [
         ("0xc329D02fd8CB2fc13aa919005aF46320794a8629", 75634),  # v1.4.1
         ("0xDAec33641865E4651fB43181C6DB6f7232Ee91c2", 13501),  # v1.3.0
+    ],
+    EthereumNetwork.SOPHON_ZKSYNC_OS_TESTNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 83239),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 20052),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 20697),  # v1.4.1
     ],
     EthereumNetwork.PLASMA_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 517662),  # v1.3.0
