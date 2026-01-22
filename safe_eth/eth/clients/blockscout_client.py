17f20ab30737d5b1ae7dc6688d5549aed07af439
Sebastián Galiano <sgaliano@gmail.com>
2026-01-22 21:59:59 +0300

    Add Ault Blockchain Testnet addresses for version 1.3.0 and 1.4.1

--- safe_eth/eth/clients/blockscout_client.py
+++ safe_eth/eth/clients/blockscout_client.py
@@ -152,6 +152,7 @@ class BlockscoutClient:
         EthereumNetwork.MANTRACHAIN_MAINNET: "https://blockscout.mantrascan.io/api/v2/",
         EthereumNetwork.DOGEOS_CHIKYU_TESTNET: "https://blockscout.testnet.dogeos.com/api/v2/",
         EthereumNetwork.ALPEN_TESTNET: "https://explorer.pectra-testnet.alpenlabs.io/api/v2/",
+        EthereumNetwork.AULT_BLOCKCHAIN_TESTNET: "https://test-evm-explorer.cloud.aultblockchain.xyz/api/v2/",
     }
 
     def __init__(
