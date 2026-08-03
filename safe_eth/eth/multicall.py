25dc5f7a473daa456a0e7dea95312cd7a7ef0843
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2026-08-03 14:05:12 +0300

    feat: add NEO_X_MAINNET safe contracts and multicall3

--- safe_eth/eth/multicall.py
+++ safe_eth/eth/multicall.py
@@ -352,6 +352,7 @@ class Multicall(ContractBase):
         EthereumNetwork.LITVM_LITEFORGE_TESTNET: "0xcA11bde05977b3631167028862bE2a173976CA11",
         EthereumNetwork.MIZUHIKI_TESTNET_AWAJI: "0xcA11bde05977b3631167028862bE2a173976CA11",
         EthereumNetwork.GRAVITY_L1: "0xcA11bde05977b3631167028862bE2a173976CA11",
+        EthereumNetwork.NEO_X_MAINNET: "0xcA11bde05977b3631167028862bE2a173976CA11",
     }
 
     def __init__(
